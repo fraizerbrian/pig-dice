bc23d54ca90117474b559545fd8d1ffd328945cc
fraizer <fraizerkilonzo@gmail.com>
2018-07-20 16:51:11 +0300

    modified js

--- js/scripts.js
+++ js/scripts.js
@@ -27,8 +27,38 @@ $().ready(function(){
         $("#die-roll-2").empty();
 
         $(".start-menu").show();
-    })
+    });
+
+    $("#player1-roll").click(function(event){
+        player1.roll = throwdice();
+        $("#die-roll-1").text(player1.roll);
+        player1.rollone();
+        $("#round-total-1").text(player1.tempscore);
+    });
+    $("#player2-roll").click(function(event){
+        player2.roll = throwdice();
+        $("#die-roll-2").text(player2.roll);
+        player2.rollone();
+        $("#round-total-2").text(player2.tempscore);
+    });
+    
+    $("#player1-pass").click(function(event){
+        player1.hold();
+        $("#total-score-1").text(player1.totalscore);
+        $("#round-total-1").empty();
+        $("#die-roll-1").empty();
+        player1.winnerCheck();
+    });
+    $("#player2-pass").click(function(event){
+        player1.hold();
+        $("#total-score-2").text(player2.totalscore);
+        $("#round-total-2").empty();
+        $("#die-roll-2").empty();
+        player2.winnerCheck();
+    });
 });
+
+
 
 var player1="";
 var player2="";
@@ -42,4 +72,30 @@ function Player(turn){
     this.totalscore=0;
     this.turn= turn;
     this.playerName;
+}
+
+Player.prototype.rollone=function(){
+    if (this.roll ===1){
+        this.tempscore = 0;
+        alert("Ooooppsss!" + this.playerName +", you rolled a 1!! Your Turn Is Over!")
+    } 
+    else{
+        this.tempscore += this.roll;
+    }
+}
+Player.prototype.pass= function(){
+    this.totalscore +=this.tempscore;
+    this.tempscore = 0;
+    alert(this.playerName +",your turn is over, pass the mouse!");
+}
+Player.prototype.newGame =function(){
+    if(this.totalscore = 100){
+        alert(this.playerName + "You are the Winner!!!");
+    }
+}
+Player.prototype.newGame = function(){
+    this.roll= 0;
+    this.tempscore=0;
+    this.totalscore=0;
+    this.playerName ="";
 }
